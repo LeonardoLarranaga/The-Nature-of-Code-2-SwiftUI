51467c4e7e777d7d8efe82c4f9ef566740e072cc
Leonardo Larrañaga <luis.larranaga@uabc.edu.mx>
2025-02-23 17:36:46 -0800

    0.4

---
+++ Chapter 0/0.4 - A Gaussian Distribution.swift
@@ -0,0 +1,38 @@
+//
+//  0.4 - A Gaussian Distribution.swift
+//  The Nature of Code
+//
+//  Created by Leonardo Larrañaga on 2/23/25.
+//
+
+import SwiftUI
+import GameplayKit
+
+struct GaussianDistribution: View {
+    
+    @State var circlesX: [Int] = []
+    @State var timer: Timer?
+    
+    var body: some View {
+        Canvas { context, size in
+            for x in circlesX {
+                let circle = Path(ellipseIn: CGRect(x: x, y: Int(size.height) / 2, width: 25, height: 25))
+                context.fill(circle, with: .color(.black.opacity(0.3)))
+            }
+        }
+        .onAppear {
+            timer = Timer.scheduledTimer(withTimeInterval: 1/30, repeats: true) { _ in
+                DispatchQueue.main.async {
+                    let random = GKGaussianDistribution(randomSource: GKRandomSource(), mean: 320, deviation: 60)
+                    circlesX.append(random.nextInt())
+                }
+            }
+        }
+        .onDisappear(perform: timer?.invalidate)
+        .navigationTitle("A Gaussian Distribution")
+    }
+}
+
+#Preview {
+    GaussianDistribution()
+}
--- ContentView.swift
+++ ContentView.swift
@@ -8,6 +8,7 @@ struct ContentView: View {
                     NavigationLink("0.1 - A Traditional Random Walk", destination: TraditionalRandomWalk())
                     NavigationLink("0.2 - A Random-Number Distribution", destination: RandomNumberDistribution())
                     NavigationLink("0.3 - A Walker That Tends to Move to the Right", destination: WalkerTendsMoveRight())
+                    NavigationLink("0.4 - A Gaussian Distribution", destination: GaussianDistribution())
                 }
             }
             .formStyle(.grouped)
